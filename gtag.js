de36d2dd4fde93218d537597fa9539c9d7a94c40
Ayush Raj <ayushraj1024@gmail.com>
2025-08-08 23:10:14 +0530

    Add google ads gtag

---
+++ gtag.js
@@ -0,0 +1,12 @@
+(function() {
+  var script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = "https://www.googletagmanager.com/gtag/js?id=AW-747450784";
+  document.head.appendChild(script1);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'AW-747450784');
+})();
